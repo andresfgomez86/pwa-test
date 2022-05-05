f041c14e9af4815e9a2828e92136b98f653fcea0
Andres Gomez <andresfgomez86@gmail.com>
2022-05-05 18:31:10 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              --
+            </Typography>
+            <Typography variant="body2">
+              Healthcare, meet the operating system
+            </Typography>
+            <Typography variant="body2">
+              designed just for you.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
       </header>
     </div>
   );
